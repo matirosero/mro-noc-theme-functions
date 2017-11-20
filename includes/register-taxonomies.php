9ac5998928e40cb868aa149323e5425526488c10
MatiRosero <gekidasa@gmail.com>
2017-11-20 15:14:39 -0600

    add board tax

--- includes/register-taxonomies.php
+++ includes/register-taxonomies.php
@@ -13,8 +13,6 @@ add_action( 'init', 'mro_cit_register_tax' );
 /* Register taxonomies */
 function mro_cit_register_tax() {
 
-
-
 	// Add LOCATION taxonomy, hierarchical (like tags)
 	$labels = array(
 		'name'                       => _x( 'Locations', 'taxonomy general name', 'textdomain' ),
@@ -114,4 +112,37 @@ function mro_cit_register_tax() {
 
 	register_taxonomy( 'noc_type', 'noc_project', $args );
 
+
+
+	// Add BOARDS taxonomy, NOT hierarchical (like tags)
+	$labels = array(
+		'name'                       => _x( 'Boards', 'taxonomy general name', 'textdomain' ),
+		'singular_name'              => _x( 'Board', 'taxonomy singular name', 'textdomain' ),
+		'search_items'               => __( 'Search Boards', 'textdomain' ),
+		'popular_items'              => __( 'Popular Boards', 'textdomain' ),
+		'all_items'                  => __( 'All Boards', 'textdomain' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Board', 'textdomain' ),
+		'update_item'                => __( 'Update Board', 'textdomain' ),
+		'add_new_item'               => __( 'Add New Board', 'textdomain' ),
+		'new_item_name'              => __( 'New Board Name', 'textdomain' ),
+		'separate_items_with_commas' => __( 'Separate boards with commas', 'textdomain' ),
+		'add_or_remove_items'        => __( 'Add or remove boards', 'textdomain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used boards', 'textdomain' ),
+		'not_found'                  => __( 'No boards found.', 'textdomain' ),
+		'menu_name'                  => __( 'Boards', 'textdomain' ),
+	);
+
+	$args = array(
+		'hierarchical'          => false,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'tableros' ),
+	);
+
+	register_taxonomy( 'noc_board', 'noc_project', $args );
 }
